8dd87bec2e401376f99d97cb059f9d5c89d63282
ibe-denaux <70886156+ibe-denaux@users.noreply.github.com>
2021-06-09 12:18:09 +0200

    Give feedback

--- python-sum-of-multiples/sum_of_multiples.py
+++ python-sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,9 @@
 def sum_of_multiples(number_upto: int, multiple_number: list) -> int:
+    # COACHES' NOTE: I feel like the functionality could be divided into two functions to avoid such deep indentation.
     total = 0
+    # COACHES' NOTE: i? 
     for i in range(0, number_upto):
+        # COACHES' NOTE: You don't a flag for this. Break statements would be better.
         flag = False
         for number in multiple_number:
             if number == 0:
@@ -13,3 +16,5 @@ def sum_of_multiples(number_upto: int, multiple_number: list) -> int:
 
 if __name__ == '__main__':
     sum_of_multiples(100, [3, 5])
+
+# COACHES' NOTE: decent code, but could use more functions to help organize.
